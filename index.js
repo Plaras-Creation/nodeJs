ac6f993f6df6394e22e3d997f89833ed06480081
Veeramanikandan M <plaraoffcl@gmail.com>
2024-11-27 10:55:22 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.json())
+app.get('/', (req, res) => {
+    console.log(req.query)
+    res.send('veera')
+})
+app.post('/test/:id', (req, res) => {
+    console.log(req.body)
+    const{sample} = req.body
+    res.json({"test":sample})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+// })
